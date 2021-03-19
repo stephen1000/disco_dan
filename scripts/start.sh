30f6cc5002149f65af3202929c84d62c7f68cf3c
Stephen Lowery <stephen.e.lowery@gmail.com>
2021-03-18 23:46:55 -0400

    +postgres

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
 # Creates necessary tables in specified db
+echo "SELECT 'CREATE DATABASE disco_dan' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'disco_dan')\gexec" | psql
+echo "SELECT 'CREATE DATABASE disco_dan_test' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'disco_dan_test')\gexec" | psql
 disco_dan initdb
 
 # Initiates the discord bot
