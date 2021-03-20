b424e9bc0f4c2084d829571c7976e2f0d1936517
stephen1000 <stephen.e.lowery@gmail.com>
2021-03-20 13:47:32 -0400

    switched to secrets

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
 # Load secrets from /run
-DISCO_DAN_CONN_STR=$(cat /run/secrets/disco_dan_conn_str)
-TEST_CONN_STR=$(cat /run/secrets/test_conn_str)
+export DISCO_DAN_CONNECTION_STRING=$(cat /run/secrets/disco_dan_conn_str)
+export TEST_CONNECTION_STRING=$(cat /run/secrets/test_conn_str)
+
 
 # Creates necessary tables in specified db
 echo "SELECT 'CREATE DATABASE disco_dan' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'disco_dan')\gexec" | psql
