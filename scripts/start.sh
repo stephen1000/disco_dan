3454bc75f691b6ee125af4ad65d5c05647ebe7f2
Stephen Lowery <stephen.e.lowery@gmail.com>
2021-03-20 13:20:45 -0400

    moved con str to secrets

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+# Load secrets from /run
+DISCO_DAN_CONN_STR=$(cat /run/secrets/disco_dan_conn_str)
+TEST_CONN_STR=$(cat /run/secrets/test_conn_str)
+
 # Creates necessary tables in specified db
 echo "SELECT 'CREATE DATABASE disco_dan' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'disco_dan')\gexec" | psql
 echo "SELECT 'CREATE DATABASE disco_dan_test' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'disco_dan_test')\gexec" | psql
